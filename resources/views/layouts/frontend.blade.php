b1efc3c07f5c5096ff6597bd4f71c54be79eb35b
kj6560 <kj6560@gmail.com>
2025-01-14 22:07:26 +0530

    a

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -96,7 +96,7 @@
         <div class="row gy-4">
           <div class="col-lg-4 col-md-6 footer-about">
             <a href="index.html" class="logo d-flex align-items-center">
-              <span class="sitename">HeroBiz</span>
+              <span class="sitename">{{env('Company_Name')}}</span>
             </a>
             <div class="footer-contact pt-3">
               <p>A108 Adam Street</p>
