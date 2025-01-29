4ece98917cf45af921ac1e2245ac61d5401cbf61
kj6560 <kj6560@gmail.com>
2025-01-29 15:14:26 +0530

    pages

--- app/Http/Controllers/frontend/SiteController.php
+++ app/Http/Controllers/frontend/SiteController.php
@@ -11,4 +11,22 @@ class SiteController extends Controller
     {
         return view('frontend.index');
     }
+    public function about(Request $request){
+        return view('frontend.about');
+    }
+    public function contactUs(Request $request){
+        return view('frontend.contact_us');
+    }
+    public function blog(Request $request){
+        return view('frontend.blog');
+    }
+    public function blogDetails(Request $request){
+        return view('frontend.blogDetails');
+    }
+    public function portfolio(Request $request){
+        return view('frontend.portfolio');
+    }
+    public function services(Request $request){
+        return view('frontend.services');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/',[SiteController::class,'index'])->name('home');
 Route::get('/contactUs',[SiteController::class,'contactUs'])->name('contactUs');
 Route::get('/about',[SiteController::class,'about'])->name('about');
 Route::get('/blog',[SiteController::class,'blog'])->name('blog');
+Route::get('/blogDetails',[SiteController::class,'blogDetails'])->name('blogDetails');
 Route::get('/services',[SiteController::class,'services'])->name('services');
 Route::get('/portfolio',[SiteController::class,'portfolio'])->name('portfolio');
