95a4fa471e8002b7077e35c9ce7192629cbf16f1
kj6560 <kj6560@gmail.com>
2025-02-02 23:25:58 +0530

    abc

--- resources/views/backend/settings/generalSettings.blade.php
+++ resources/views/backend/settings/generalSettings.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.backend')
 @section('content')
 <div class="pagetitle">
-    <h1>Dashboard</h1>
+    <h1>General Settings</h1>
     <nav>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
@@ -10,4 +10,16 @@
         </ol>
     </nav>
 </div><!-- End Page Title -->
+<section class="section dashboard">
+    <div class="row">
+
+        <!-- Left side columns -->
+        <div class="col-lg-8">
+            <div class="row">
+
+            </div>
+        </div><!-- End Left side columns -->
+
+    </div>
+</section>
 @endsection
