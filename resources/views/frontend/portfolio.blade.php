0e1ea84dcf1462b1684b12af5c21c7fdbc0b5258
kj6560 <kj6560@gmail.com>
2025-05-19 16:40:08 +0530

    a

--- resources/views/frontend/portfolio.blade.php
+++ resources/views/frontend/portfolio.blade.php
@@ -1,54 +1,56 @@
 @extends('layouts.frontend')
 @section('content')
-<!-- Portfolio Section -->
-<section id="portfolio" class="portfolio section">
+  <!-- Portfolio Section -->
+  <section id="portfolio" class="portfolio section">
 
-  <!-- Section Title -->
-  <div class="container section-title" data-aos="fade-up">
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
     <h2>Portfolio</h2>
     <p>Some of our completed and ongoing projects are listed here.</p>
-  </div><!-- End Section Title -->
+    </div><!-- End Section Title -->
 
-  <div class="container-fluid">
+    <div class="container-fluid">
 
     <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
 
       <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
-        <li data-filter="*" class="filter-active">All</li>
-        <li data-filter=".filter-app">App</li>
-        <li data-filter=".filter-product">Website</li>
-        <li data-filter=".filter-branding">Backend Utility</li>
+      <li data-filter="*" class="filter-active">All</li>
+      <li data-filter=".filter-app">App</li>
+      <li data-filter=".filter-product">Website</li>
+      <li data-filter=".filter-branding">Backend Utility</li>
 
       </ul><!-- End Portfolio Filters -->
 
       <div class="row g-0 isotope-container" data-aos="fade-up" data-aos-delay="200">
 
-        @foreach($portfolios as $portfolio)
-        @php
-        $type = '';
-        if ($portfolio->work_type == 1) {
-        $type = 'product';
-        } elseif ($portfolio->work_type == 2) {
-        $type = 'app';
-        } elseif ($portfolio->work_type == 3) {
-        $type = 'binding';
-        }
-        @endphp
+      @foreach($portfolios as $portfolio)
+      @php
+      $type = '';
+      if ($portfolio->work_type == 1) {
+      $type = 'product';
+      } elseif ($portfolio->work_type == 2) {
+      $type = 'app';
+      } elseif ($portfolio->work_type == 3) {
+      $type = 'binding';
+      }
+      @endphp
 
-        <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item isotope-item filter-{{ $type }}">
-          <div class="portfolio-content h-100">
-            <img src="{{ url('storage') }}/{{ $portfolio->work_image }}" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <a href="{{ asset('theme') }}/img/portfolio/app-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
-                <i class="bi bi-zoom-in"></i>
-              </a>
-              <a href="portfolio-details.html" title="More Details" class="details-link">
-                <i class="bi bi-link-45deg"></i>
-              </a>
-            </div>
-          </div>
-        </div><!-- End Portfolio Item -->
-        @endforeach
+      <div class="col-xl-3 col-lg-4 col-md-6 portfolio-item isotope-item filter-{{ $type }}">
+        <div class="portfolio-content h-100">
+        <img src="{{ url('storage') }}/{{ $portfolio->work_image }}" class="img-fluid" alt="">
+        <div class="portfolio-info">
+        <a href="{{ url('storage') }}/{{ $portfolio->work_image }}" data-gallery="portfolio-gallery-app"
+        class="glightbox preview-link">
+        <i class="bi bi-zoom-in"></i>
+        </a>
+        <a href="{{ $portfolio->work_url }}" target="_blank" title="More Details" class="details-link">
+        <i class="bi bi-link-45deg"></i>
+        </a>
+        </a>
+        </div>
+        </div>
+      </div><!-- End Portfolio Item -->
+    @endforeach
 
 
 
@@ -58,7 +60,7 @@
 
     </div>
 
-  </div>
+    </div>
 
-</section><!-- /Portfolio Section -->
+  </section><!-- /Portfolio Section -->
 @endsection
