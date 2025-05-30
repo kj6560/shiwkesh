06858e0b042fe5be633fa0d9d6f756a36806178f
kj6560 <kj6560@gmail.com>
2025-05-30 15:13:05 +0530

    abc

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('sitemap:generate')->everyMinute()->withoutOverlapping()->runInBackground();
-Schedule::command('command:backup')->everyOddHour()->withoutOverlapping()->runInBackground();
+Schedule::command('command:backup')->hourly()->withoutOverlapping()->runInBackground();
